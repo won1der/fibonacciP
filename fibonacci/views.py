74495b66cdf5271ef4d87c0f952906d50719fb30
won1der <40011305+won1der@users.noreply.github.com>
2019-11-02 18:52:50 +0530

    Update views.py

--- fibonacci/views.py
+++ fibonacci/views.py
@@ -26,7 +26,7 @@ def fibonacci(request):
             return HttpResponse('Value of n is incorrect')
         answer = calculateFibnacci(int(nth))
         end_t=time.time()-start_t
-        s=""+nth+"th Fibonacci number is "+str(answer)+".  Time required in computation is"+str(end_t)
+        s=""+nth+"th Fibonacci number is "+str(answer)+".\n Time required in computation is "+str(end_t)+" sec"
         return HttpResponse(s)
 
     else:
