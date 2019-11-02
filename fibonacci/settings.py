b848624422b9eced358c34a2381712000a3edb6c
Vishal Mishra <shas1hwat@gmail.com>
2019-11-02 10:11:58 +0530

    added files

--- fibonacci/settings.py
+++ fibonacci/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lnz@yg3jie0)2==b*-@4(z5a6$pdfm74x)(vali!(^ebs--0xp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static")
